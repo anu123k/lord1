007b7732d3ba2aa3ff6c9c74498b0b1160604ec2
lenovo <anudeep362799@gmail.com>
2022-01-04 11:29:42 +0530

    added maven project

--- mYPro/src/mYPro/thresclass.java
+++ mYPro/src/mYPro/thresclass.java
@@ -4,8 +4,7 @@ public class thresclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-                      System.out.println("hello world ");
-                      System.out.println("hello world ");
+                     
                       
                       
 		
